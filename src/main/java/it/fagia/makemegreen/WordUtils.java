d6f528d9ed341d3645d2618c8a8cd26c88a518d8
Matteo Cajani <cmatteo@thoughtworks.com>
2018-10-08 23:47:39 +0200

    Word utils tests hello world

---
+++ src/main/java/it/fagia/makemegreen/WordUtils.java
@@ -0,0 +1,20 @@
+package it.fagia.makemegreen;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+class WordUtils {
+
+    private String word;
+
+    WordUtils(String word) {
+        this.word = word;
+    }
+
+    boolean isValid() {
+        throw new NotImplementedException("Implement me!"); // TODO
+    }
+
+    String getWord() {
+        return word;
+    }
+}
