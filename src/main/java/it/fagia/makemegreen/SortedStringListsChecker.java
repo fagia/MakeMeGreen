64da493b3f0c52f401ba63709a7886229730244f
Matteo Cajani <cmatteo@thoughtworks.com>
2018-10-08 23:46:16 +0200

    Simplify test

--- src/main/java/it/fagia/makemegreen/SortedStringListsChecker.java
+++ src/main/java/it/fagia/makemegreen/SortedStringListsChecker.java
@@ -5,18 +5,18 @@ import org.apache.commons.lang3.NotImplementedException;
 import java.util.List;
 
 class SortedStringListsChecker {
+
     private List<String> stringList;
 
     SortedStringListsChecker(List<String> stringList) {
         this.stringList = stringList;
     }
 
-    @Override
-    public String toString() {
-        return stringList == null ? "null" : stringList.toString();
+    boolean isSorted() {
+        throw new NotImplementedException("Implement me!"); // TODO
     }
 
-    boolean isSorted() {
-        throw new NotImplementedException("Implement me!");
+    List<String> getStringList() {
+        return stringList;
     }
 }
--- src/test/java/it/fagia/makemegreen/SortedStringListsCheckerTests.java
+++ src/test/java/it/fagia/makemegreen/SortedStringListsCheckerTests.java
@@ -1,14 +1,17 @@
 package it.fagia.makemegreen;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 @DisplayName("Sorted string lists checker")
 class SortedStringListsCheckerTests {
@@ -16,59 +19,54 @@ class SortedStringListsCheckerTests {
     @TestFactory
     @DisplayName("should check that these lists are sorted")
     Stream<DynamicTest> sorted() {
-        List<String> s1 = Arrays.asList("a) string", "b) string", "c) string");
-        List<String> s2 = Arrays.asList("1. string", "2. string", "3. string");
-        List<String> s3 = Collections.singletonList("only one string");
-        return buildSortedTestStream(s1, s2, s3);
+        List<String> s1 = asList("a) string", "b) string", "c) string");
+        List<String> s2 = asList("1. string", "2. string", "3. string");
+        List<String> s3 = singletonList("only one string");
+        return buildTestStreamForSorted(s1, s2, s3);
     }
 
     @TestFactory
     @DisplayName("should check that these lists aren't sorted")
     Stream<DynamicTest> notSorted() {
-        List<String> u1 = Arrays.asList("c) string", "a) string", "b) string");
-        List<String> u2 = Arrays.asList("2. string", "1. string", "3. string");
-        return buildNotSortedTestStream(u1, u2, null);
+        List<String> u1 = asList("c) string", "a) string", "b) string");
+        List<String> u2 = asList("2. string", "1. string", "3. string");
+        return buildTestStreamForNotSorted(u1, u2, null);
     }
 
     @TestFactory
     @DisplayName("should check that these lists are sorted in a case insensitive way")
     Stream<DynamicTest> sortedCaseInsensitive() {
-        List<String> i1 = Arrays.asList("a) string", "B) string", "c) string", "D) string");
-        List<String> i2 = Arrays.asList("A) string", "b) string", "C) string", "d) string");
-        List<String> i3 = Arrays.asList("A) string", "B) string", "C) string", "D) string");
-        return buildSortedTestStream(i1, i2, i3);
+        List<String> i1 = asList("a) string", "B) string", "c) string", "D) string");
+        List<String> i2 = asList("A) string", "b) string", "C) string", "d) string");
+        List<String> i3 = asList("A) string", "B) string", "C) string", "D) string");
+        return buildTestStreamForSorted(i1, i2, i3);
     }
 
     @SafeVarargs
-    private final Stream<DynamicTest> buildSortedTestStream(List<String>... sortedStringLists) {
+    private final Stream<DynamicTest> buildTestStreamForSorted(List<String>... sortedStringLists) {
         return buildTestStream(true, sortedStringLists);
     }
 
     @SafeVarargs
-    private final Stream<DynamicTest> buildNotSortedTestStream(List<String>... unsortedStringLists) {
+    private final Stream<DynamicTest> buildTestStreamForNotSorted(List<String>... unsortedStringLists) {
         return buildTestStream(false, unsortedStringLists);
     }
 
     @SafeVarargs
     private final Stream<DynamicTest> buildTestStream(boolean isSorted, List<String>... stringLists) {
-        return Arrays.stream(stringLists)
-                .map(stringList -> buildTestData(stringList, isSorted))
-                .map(data -> DynamicTest.dynamicTest(
-                        data.objectUnderTest.toString(),
-                        () -> Assertions.assertEquals(data.isSorted, data.objectUnderTest.isSorted())));
+        return stream(stringLists)
+                .map(TestData::new)
+                .map(t -> dynamicTest(t.toString(), () -> assertEquals(isSorted, t.isSorted())));
     }
 
-    private TestData buildTestData(List<String> strings, boolean isSorted) {
-        return new TestData(new SortedStringListsChecker(strings), isSorted);
-    }
+    private class TestData extends SortedStringListsChecker {
+        TestData(List<String> stringList) {
+            super(stringList);
+        }
 
-    private class TestData {
-        private SortedStringListsChecker objectUnderTest;
-        private boolean isSorted;
-
-        TestData(SortedStringListsChecker objectUnderTest, boolean isSorted) {
-            this.objectUnderTest = objectUnderTest;
-            this.isSorted = isSorted;
+        @Override
+        public String toString() {
+            return getStringList() == null ? "<null>" : getStringList().toString();
         }
     }
 }
