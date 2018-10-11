f17c8a89098ed8b25804701c9cbf54ed5967ac1e
Matteo Cajani <cmatteo@thoughtworks.com>
2018-10-11 08:44:36 +0200

    add support for (future) different than "equals" assertions

--- src/test/java/it/fagia/makemegreen/TestBase.java
+++ src/test/java/it/fagia/makemegreen/TestBase.java
@@ -2,22 +2,39 @@ package it.fagia.makemegreen;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicTest;
 
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class TestBase<T, P, R> {
 
     @SafeVarargs
-    final Stream<DynamicTest> buildTestStream(Function<P, T> dataBuilder, Function<T, R> actionExecutor, Pair<P, R>... testPairs) {
+    final Stream<DynamicTest> buildTestStream(
+            Function<P, T> dataBuilder,
+            Function<T, R> actionExecutor,
+            Pair<P, R>... testPairs) {
+        return buildTestStream(dataBuilder, actionExecutor, Assertions::assertEquals, testPairs);
+    }
+
+    @SafeVarargs
+    private final Stream<DynamicTest> buildTestStream(
+            Function<P, T> dataBuilder,
+            Function<T, R> actionExecutor,
+            BiConsumer<R, R> assertion,
+            Pair<P, R>... testPairs) {
         return stream(testPairs)
                 .map(testPair -> new ImmutablePair<>(dataBuilder.apply(testPair.getLeft()), testPair.getRight()))
                 .map(testPair -> dynamicTest(testPair.getLeft().toString(),
-                        () -> assertEquals(testPair.getRight(), actionExecutor.apply(testPair.getLeft()))));
+                        () -> {
+                            R expected = testPair.getRight();
+                            R actual = actionExecutor.apply(testPair.getLeft());
+                            assertion.accept(expected, actual);
+                        }));
     }
 }
